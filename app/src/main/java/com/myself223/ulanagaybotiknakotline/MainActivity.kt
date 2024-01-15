8313e56452c64d83c2ffba6fac6603b4792ce979
Arsenbotik228 <147414597+Arsenbotik228@users.noreply.github.com>
2024-01-15 20:51:28 +0600

    1.2

--- app/src/main/java/com/myself223/ulanagaybotiknakotline/MainActivity.kt
+++ app/src/main/java/com/myself223/ulanagaybotiknakotline/MainActivity.kt
@@ -2,13 +2,18 @@ package com.myself223.ulanagaybotiknakotline
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
+import com.myself223.ulanagaybotiknakotline.databinding.ActivityMainBinding
+import com.myself223.ulanagaybotiknakotline.databinding.ItemNoteBinding
 
 class MainActivity : AppCompatActivity() {
-
+private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         supportFragmentManager.setFragmentResultListener(
             "new_note", this
--- app/src/main/java/com/myself223/ulanagaybotiknakotline/NoteAdapter.kt
+++ app/src/main/java/com/myself223/ulanagaybotiknakotline/NoteAdapter.kt
@@ -1,7 +1,5 @@
 package com.myself223.ulanagaybotiknakotline
 
-import android.content.Intent
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -9,49 +7,45 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.myself223.ulanagaybotiknakotline.databinding.ItemNoteBinding
 
 
+
+
+// NoteAdapter
 class NoteAdapter(
-    private val  listener: Clickable)
-    : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
-
-
-
-
+    private val listener: Clickable
+) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
 
     private var list = arrayListOf<Notes>()
-    private var inflater: LayoutInflater? = null
+
 
     fun addNote(list: ArrayList<Notes>) {
         this.list = list
         notifyDataSetChanged()
     }
-    fun changeNote(note: Notes, position: Int){
+    fun changeNote(note: Notes, position: Int) {
         list[position] = note
         notifyItemChanged(position)
     }
-    fun getList(): List<Notes>?{
+
+    fun getList(): List<Notes>? {
         return list
     }
 
     fun sortNotes() {
         list.sortBy { it.title }
-
-
         notifyDataSetChanged()
     }
 
-    fun removeNotes(position:Int){
-        list.remove(list[position])
-
-
-
+    fun removeNotes(position: Int) {
+        if (position in 0 until list.size) {
+            list.removeAt(position)
+            notifyDataSetChanged()
+        }
     }
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        inflater = LayoutInflater.from(parent.context)
-        val binding = ItemNoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemNoteBinding.inflate(inflater, parent, false)
         return NoteViewHolder(binding)
-
     }
 
     override fun getItemCount(): Int {
@@ -59,46 +53,31 @@ class NoteAdapter(
     }
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-
-
         holder.onBind(list[position])
-        holder.binding.itemBtnEdit.setOnClickListener{
+        holder.binding.itemBtnEdit.setOnClickListener {
             listener.edit(holder.adapterPosition)
         }
         holder.binding.itemBtnDelete.setOnClickListener {
             listener.delete(holder.adapterPosition)
-
         }
         holder.binding.itemBtnShare.setOnClickListener {
-            val share = Intent(Intent.ACTION_SEND)
-            share.type = "item_tv_title/*"
-            share.putExtra(Intent.EXTRA_STREAM)
-
-
+            listener.share(list[position])
         }
-
-
     }
 
     class NoteViewHolder constructor(val binding: ItemNoteBinding) :
-        ViewHolder(binding.root) {
+        RecyclerView.ViewHolder(binding.root) {
 
         fun onBind(note: Notes) {
-
-
             binding.itemTvTitle.text = note.title
             binding.itemTvDes.text = note.desc
             binding.itemTvDate.text = note.date
-
         }
     }
-    interface  Clickable{
+
+    interface Clickable {
         fun edit(position: Int)
         fun delete(position: Int)
+        fun share(note: Notes)
     }
-    }
-
-
-private fun Intent.putExtra(extraStream: Any) {
-
 }
